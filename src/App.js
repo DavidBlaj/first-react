ef524b93e52c511c45cb2367411703eac7e68690
David <blaj.david1@gmail.com>
2023-11-29 18:05:03 +0100

    1.0     Simple app built in React where i learned the following:        - state and hooks        - custom components        - Router        - useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState, useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieCard from "./components/MovieCard";
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=e2c36544';
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [searchTerm]);
+
+    return (
+        <div className="app">
+            <h1>Movie Land</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    // e = event
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img src={SearchIcon}
+                     alt="search"
+                     onClick={() => searchMovies(searchTerm)}
+
+                />
+            </div>
+
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+const MovieDetails = ( {} ) => {
+    return (
+       <div>This is the details page</div>
+    )
+}
+
+export default MovieDetails;
